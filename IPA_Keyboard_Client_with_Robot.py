ef58791f4d280fb35bd390c3fc959a7637e36966
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-18 18:37:21 -0400

    It *works*!!!  Though not fully functional

--- IPA_Keyboard_Client_with_Robot.py
+++ IPA_Keyboard_Client_with_Robot.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3.11
 
 import socket
-
+from pynput.keyboard import Controller
 from table import TABLE
+# from time import sleep # just for testing
 
 HOST = "phonetics.ling.udel.edu"
 PORT = 8000
 
+keyboard = Controller()
+
 def main():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
-		s.sendall(b'`asdf')
+		s.sendall(b'`asdf') # just for testing
 		while True:
 			dataIn = int.from_bytes(s.recv(1), "big")
 			if dataIn >= len(TABLE):
@@ -21,8 +24,11 @@ def main():
 						print("You've been timed out.")
 					case 255 | _:
 						print("An unknown error ocurred.")
-				return
+				continue
 			print(dataIn, TABLE[dataIn])
+			# sleep(2) # just for testing
+			keyboard.press(TABLE[dataIn])
+			keyboard.release(TABLE[dataIn])
 
 if __name__ == "__main__":
 	main()
