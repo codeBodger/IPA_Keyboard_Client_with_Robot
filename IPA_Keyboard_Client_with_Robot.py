a8b831fa18ef73cecc95d6235cab41b6496fef9c
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-19 15:54:59 -0400

    generate a linking code

--- IPA_Keyboard_Client_with_Robot.py
+++ IPA_Keyboard_Client_with_Robot.py
@@ -3,6 +3,7 @@
 import socket
 from pynput.keyboard import Controller
 from table import TABLE
+from random import random as rand
 # from time import sleep # just for testing
 
 HOST = "phonetics.ling.udel.edu"
@@ -13,7 +14,10 @@ keyboard = Controller()
 def main():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
-		s.sendall(b'`asdf') # just for testing
+		# s.sendall(b'`asdf') # just for testing
+		linking_code = str(rand())[2:8]
+		print(f"Linking code: {linking_code}")
+		s.sendall(b'`' + bytes(linking_code, 'utf-8'))
 		while True:
 			dataIn = int.from_bytes(s.recv(1), "big")
 			if dataIn >= len(TABLE):
@@ -22,6 +26,8 @@ def main():
 						print("Connection successful.")
 					case 253:
 						print("You've been timed out.")
+					case 252:
+						print("Linking successful.")
 					case 255 | _:
 						print("An unknown error ocurred.")
 				continue
