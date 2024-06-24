488d072285187bbdfef4a0d2b649ab1fa029f237
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-06-24 12:35:40 -0400

    `tkinter` GUI now!

--- IPA_Keyboard_Client_with_Robot.py
+++ IPA_Keyboard_Client_with_Robot.py
@@ -5,36 +5,67 @@ from pynput.keyboard import Controller
 from table import TABLE
 from random import random as rand
 # from time import sleep # just for testing
+import tkinter as tk
+from threading import Thread
 
 HOST = "phonetics.ling.udel.edu"
 PORT = 8000
 
-keyboard = Controller()
+KEYBOARD = Controller()
+
+APP = tk.Tk("IPA Keyboard Client")
+
+MESSAGE = tk.StringVar()
+KEY = tk.StringVar()
+
+def stop():
+	APP.destroy()
+	client_socket.close()
+	exit()
+
+def client():
+	global client_socket
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client_socket.connect((HOST, PORT))
+	# s.sendall(b'`asdf') # just for testing
+	linking_code = str(rand())[2:8]
+	KEY.set(f"Linking code: {linking_code}")
+	client_socket.sendall(b'`' + bytes(linking_code, 'utf-8'))
+	while True:
+		try:
+			dataIn = int.from_bytes(client_socket.recv(1), "big")
+		except Exception:
+			MESSAGE.set("The socket is closed.")
+			client_socket.close()
+			return
+		if dataIn >= len(TABLE):
+			match dataIn:
+				case 254:
+					MESSAGE.set("Connection successful.")
+				case 253:
+					MESSAGE.set("You've been timed out.")
+					client_socket.close()
+					return
+				case 252:
+					MESSAGE.set("Linking successful.")
+				case 255 | _:
+					MESSAGE.set("An unknown error ocurred.")
+			continue
+		MESSAGE.set(f"Sent character {dataIn:3}: {TABLE[dataIn]}")
+		# sleep(2) # just for testing
+		KEYBOARD.press(TABLE[dataIn])
+		KEYBOARD.release(TABLE[dataIn])
+
+def app(after_start):
+	tk.Button(APP, name="stop", text='Stop', width=25, command=stop).pack()
+	tk.Button(APP, name="renew", text='Renew', width=25, command=after_start).pack()
+	tk.Label(APP, name="message", textvariable=MESSAGE).pack()
+	tk.Label(APP, name="key", textvariable=KEY).pack()
+	APP.after(0, after_start)
+	APP.mainloop()
 
 def main():
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		# s.sendall(b'`asdf') # just for testing
-		linking_code = str(rand())[2:8]
-		print(f"Linking code: {linking_code}")
-		s.sendall(b'`' + bytes(linking_code, 'utf-8'))
-		while True:
-			dataIn = int.from_bytes(s.recv(1), "big")
-			if dataIn >= len(TABLE):
-				match dataIn:
-					case 254:
-						print("Connection successful.")
-					case 253:
-						print("You've been timed out.")
-					case 252:
-						print("Linking successful.")
-					case 255 | _:
-						print("An unknown error ocurred.")
-				continue
-			print(dataIn, TABLE[dataIn])
-			# sleep(2) # just for testing
-			keyboard.press(TABLE[dataIn])
-			keyboard.release(TABLE[dataIn])
+	app(lambda: Thread(target=client, daemon=True).start())
 
 if __name__ == "__main__":
 	main()
